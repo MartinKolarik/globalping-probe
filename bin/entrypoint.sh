a77fc5ad7da8a062aebdd391783aa721a6b3a83f
Martin Kolárik <martin@kolarik.sk>
2024-04-17 13:40:18 +0200

    fix: entrypoint change

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -8,7 +8,7 @@ function run_probe() {
 }
 
 function try_update() {
-	echo "Checking for the latest version 1"
+	echo "Checking for the latest version 2"
 
 	response=$(curl -XGET -Lf -sS "https://api.github.com/repos/MartinKolarik/globalping-probe/releases/latest")
 
