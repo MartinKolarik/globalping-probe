c13507699a4f6a0aac354e4af657276c753ab08c
Martin Kolárik <martin@kolarik.sk>
2024-04-17 18:19:05 +0200

    fix: files

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -8,7 +8,7 @@ function run_probe() {
 }
 
 function try_update() {
-	echo "Checking for the latest version 6"
+	echo "Checking for the latest version 7"
 
 	response=$(curl -XGET -Lf -sS "https://data.jsdelivr.com/v1/packages/gh/MartinKolarik/globalping-probe/resolved")
 
