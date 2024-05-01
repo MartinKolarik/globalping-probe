b0aacd31eb58428c2f5f30ab8f2aad00ada89305
Martin Kolárik <martin@kolarik.sk>
2024-05-01 14:57:33 +0200

    fix: change node version

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'node:url';
 
 import { isV1HardwareDevice } from './lib/util.js';
 
-const WANTED_VERSION = 'v18.20.0';
+const WANTED_VERSION = 'v18.20.1';
 const MIN_NODE_UPDATE_MEMORY = 1e9;
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
